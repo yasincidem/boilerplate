3e644c8909737f1485dabd44d933498d096ff0a7
Yasin Çidem <yasincidem@gmail.com>
2020-09-01 20:34:36 +0300

    [NETWORK] Chucker implementation

--- app/src/main/java/com/yasincidem/boilerplate/di/ApiModule.kt
+++ app/src/main/java/com/yasincidem/boilerplate/di/ApiModule.kt
@@ -1,25 +1,45 @@
 package com.yasincidem.boilerplate.di
 
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object ApiModule {
-    const val TIME_OUT_SECONDS = 10
+    private const val TIME_OUT_SECONDS = 10
     private const val BASE_URL = "https://5f467f36e165a60016ba9c53.mockapi.io/"
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(
+                TIME_OUT_SECONDS.toLong(),
+                TimeUnit.SECONDS)
+            .readTimeout(
+                TIME_OUT_SECONDS.toLong(),
+                TimeUnit.SECONDS)
+            .addInterceptor(ChuckerInterceptor(context))
             .build()
     }
 
